4b33ca423ee6307cadfb0c263959b1e231cd9a05
Iurii Zdebskyi <iuriiz@devfair004.maas>
2020-09-11 15:03:30 -0700

    Add foreach APIs with ScalarList
    ghstack-source-id: a6f3b92ce720d04fa94056087897be3eb65710a0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/44519

---
+++ 1.py
@@ -0,0 +1,27 @@
+import torch 
+
+a = [torch.ones(1, 1, dtype=torch.int32) for _ in range(3)]
+b = [torch.ones(1, 1, dtype=torch.int32, device='cuda') for _ in range(3)]
+print(a)
+print(b)
+
+res = torch._foreach_add(a, [1, 2, 3])
+print("\nresA = ", res)
+
+torch._foreach_add_(a, [1, 2, 3])
+#print("\nresA = ", a)
+
+#res = torch._foreach_add(b, [1, 2, 3])
+#print("\n\n\n\nresB = ", res)
+
+torch._foreach_add_(b, [1, 2, 3])
+print("\nresB = ", b)
+
+
+b = [torch.ones(1, 1, dtype=torch.float, device='cuda') for _ in range(3)]
+res = torch._foreach_add(b, [1.1, 2.1, 3.1])
+print("\nresB = ", res)
+
+torch._foreach_add_(b, [1.1, 2.1, 3.1])
+print("\nresB = ", b)
+
